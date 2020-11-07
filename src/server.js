ffdd49679741d8179c72d4ec465445843b956133
Matthew Glenn <mfglenn@outlook.com>
2020-11-06 17:33:58 -0700

    Import bodyParser to properly handle request bodies as JSON.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get('/hello', (req, res) => {
     return res.send('Hello!');
+});
+
+app.post('/hello', (req, res) => {
+    return res.send(`Hello ${req.body.name}!`);
 });
 
 app.listen(8000, () => console.log('Listening on port 8000'));
