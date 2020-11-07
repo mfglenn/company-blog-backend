03813e2369cbe7f1a4b51e29117fb5f45f84cb2c
Matthew Glenn <mfglenn@outlook.com>
2020-11-06 17:49:00 -0700

    Update the server to handle the upvote post request.  Include the nodemon package to automate server refresh.

--- src/server.js
+++ src/server.js
@@ -1,22 +1,28 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+const articlesInfo = {
+    'learn-react': {
+        upvotes: 0
+    },
+    'learn-node': {
+        upvotes: 0
+    },
+    'my-thoughts-on-resumes': {
+        upvotes: 0
+    }
+}
 const app = express();
 
 //Deifine parser for request bodies
 app.use(bodyParser.json());
 
-//Define route responses from server
-app.get('/hello', (req, res) => {
-    return res.send('Hello!');
-});
+//Define enpoint responses from server
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const articleName = req.params.name; 
+    articlesInfo[articleName].upvotes += 1;
 
-app.get('/hello/:name', (req, res) => {
-    return res.send(`Hello ${req.params.name}!`);
-});
-
-app.post('/hello', (req, res) => {
-    return res.send(`Hello ${req.body.name}!`);
-});
+    res.status(200).send(`Article ${articleName} no has ${articlesInfo[articleName].upvotes} upvotes.`)
+})
 
 app.listen(8000, () => console.log('Listening on port 8000'));
