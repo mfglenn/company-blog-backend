71abb51e96c4954e228da164aea38d5ff5914ec2
Matthew Glenn <mfglenn@outlook.com>
2020-11-06 17:39:40 -0700

    Update server.js to include additional routing configuration.

--- src/server.js
+++ src/server.js
@@ -3,10 +3,16 @@ import bodyParser from 'body-parser';
 
 const app = express();
 
+//Deifine parser for request bodies
 app.use(bodyParser.json());
 
+//Define route responses from server
 app.get('/hello', (req, res) => {
     return res.send('Hello!');
+});
+
+app.get('/hello/:name', (req, res) => {
+    return res.send(`Hello ${req.params.name}!`);
 });
 
 app.post('/hello', (req, res) => {
