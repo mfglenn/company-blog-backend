b31faac51bcb39d242a273fd3e9a5940ae582bae
Matthew Glenn <mfglenn@outlook.com>
2020-11-06 18:56:06 -0700

    Add mongodb npm package to utilize MongoDB as a db for the application.

--- src/server.js
+++ src/server.js
@@ -1,20 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-const articlesInfo = {
-    'learn-react': {
-        upvotes: 0,
-        comments: []
-    },
-    'learn-node': {
-        upvotes: 0,
-        comments: []
-    },
-    'my-thoughts-on-resumes': {
-        upvotes: 0,
-        comments: []
-    }
-};
 const app = express();
 
 //Deifine parser for request bodies
