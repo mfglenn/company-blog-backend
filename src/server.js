0e5bcaf428e8ec5ca6b664913bbb635ee0792d11
Matthew Glenn <mfglenn@outlook.com>
2020-11-06 18:03:19 -0700

    Add end-point to support comment creation.

--- src/server.js
+++ src/server.js
@@ -3,15 +3,18 @@ import bodyParser from 'body-parser';
 
 const articlesInfo = {
     'learn-react': {
-        upvotes: 0
+        upvotes: 0,
+        comments: []
     },
     'learn-node': {
-        upvotes: 0
+        upvotes: 0,
+        comments: []
     },
     'my-thoughts-on-resumes': {
-        upvotes: 0
+        upvotes: 0,
+        comments: []
     }
-}
+};
 const app = express();
 
 //Deifine parser for request bodies
@@ -22,7 +25,16 @@ app.post('/api/articles/:name/upvote', (req, res) => {
     const articleName = req.params.name; 
     articlesInfo[articleName].upvotes += 1;
 
-    res.status(200).send(`Article ${articleName} no has ${articlesInfo[articleName].upvotes} upvotes.`)
+    res.status(200).send(`Article ${articleName} now has ${articlesInfo[articleName].upvotes} upvotes.`);
+})
+
+app.post('/api/articles/:name/add-comment', (req, res) => {
+    const articleName = req.params.name; 
+    const { username, text } = req.body;
+    
+    articlesInfo[articleName].comments.push({ username, text });
+
+    res.status(200).send(articlesInfo[articleName]);
 })
 
 app.listen(8000, () => console.log('Listening on port 8000'));
